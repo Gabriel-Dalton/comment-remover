73f25060c52c7568e278b0279ced0295c62750cc
Gabriel Dalton <86272315+Gabriel-Dalton@users.noreply.github.com>
2024-08-24 23:49:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function removeComments() {
+    const code = document.getElementById('inputCode').value;
+    const uncommentedCode = code
+        .replace(/\/\*[\s\S]*?\*\
+        .replace(/\/\/.*/g, ''); 
 
+    document.getElementById('outputCode').value = uncommentedCode.trim();
+}
+
+function resetForm() {
+    document.getElementById('inputCode').value = '';
+    document.getElementById('outputCode').value = '';
+}
+
+function copyToClipboard() {
+    const outputCode = document.getElementById('outputCode');
+    outputCode.select();
+    outputCode.setSelectionRange(0, 99999); 
+    document.execCommand('copy');
+    alert('Code copied to clipboard!');
+}
